ced4962ec14ab81c447a842c9cbd0533aa043434
bibhashZelp <82034778+bibhashZelp@users.noreply.github.com>
2022-02-07 12:14:51 +0530

    updated function

--- handler.js
+++ handler.js
@@ -5,7 +5,7 @@ module.exports.test = async (event) => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
+        message: 'Your function executed successfully!',
         input: event,
       },
       null,
